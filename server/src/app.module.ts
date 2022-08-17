c2da1137dea7bbbee94312e2196d522a78468ae2
NikitaStepanenko <stepanenkonikit@gmail.com>
2022-08-17 17:26:52 +0300

    Finish server

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,4 +1,22 @@
 import { Module } from '@nestjs/common';
+import { TrackModule } from './track/track.module';
+import { AlbumModule } from './album/album.module';
+import { MongooseModule } from '@nestjs/mongoose';
+import { FileModule } from './file/file.module';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import * as path from 'path';
 
-@Module({})
-export class AppModule { }
+@Module({
+  imports: [
+    MongooseModule.forRoot(
+      'mongodb+srv://admin:admin@cluster0.d7f6c.mongodb.net/?retryWrites=true&w=majority',
+    ),
+    ServeStaticModule.forRoot({
+      rootPath: path.resolve(__dirname, 'static'),
+    }),
+    TrackModule,
+    AlbumModule,
+    FileModule,
+  ],
+})
+export class AppModule {}
